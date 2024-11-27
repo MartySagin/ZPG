d91535615194b3894759ccb8dd080533abf48511
Martin Kalus <martin.kalus.st@vsb.cz>
2024-11-27 15:07:01 +0100

    Úpravička

--- ZPG_CV_1/ModelObject.h
+++ ZPG_CV_1/ModelObject.h
@@ -2,19 +2,19 @@
 
 #include "Model.h"
 
+struct Vertex {
+    float Position[3];
+    float Normal[3];
+    float Texture[2];
+    float Tangent[3];
+};
+
 class ModelObject : public Model
 {
 	private:
         GLuint EBO;
 
         int indicesCount;
-
-        struct Vertex {
-            float Position[3];
-            float Normal[3];
-            float Texture[2];
-            float Tangent[3];
-        };
 
     public:
         ModelObject();
